4d5bfcdfffb00ee2ef75b70142cfc25ea841f4d0
Burak-Altunbas <burak.altunbas@outlook.com>
2017-11-27 18:07:15 +0300

    birşeyler eklendi

--- PT.DAL/MyContext.cs
+++ PT.DAL/MyContext.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using PT.Entity.IdentityModel;
 using PT.Entity.Model;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PT.DAL
 {
